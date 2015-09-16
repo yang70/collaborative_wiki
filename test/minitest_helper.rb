d0f53d8448b8eb79f195a67e41192ba5809c31a8
Matthew Yang <yang70@gmail.com>
2015-09-16 16:38:28 -0700

    devise up and running

---
+++ test/minitest_helper.rb
@@ -0,0 +1,16 @@
+Rails.env = "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
+require "minitest/rails"
+require "minitest/rails/capybara"
+
+class ActiveSupport::TestCase
+  fixtures :all
+  # Add more helper methods to be used by all tests here...
+end
+
+class ActionController::TestCase
+  include Devise::TestHelpers
+end
+
+system 'rubocop'
