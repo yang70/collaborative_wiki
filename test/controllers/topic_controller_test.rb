8ce0cb7a48cc86416adf988f93e6c98a614e8ce1
Matthew Yang <yang70@gmail.com>
2015-09-17 13:29:11 -0700

    working tests for all features

--- test/controllers/topic_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class TopicControllerTest < ActionController::TestCase
-  def test_sanity
-    flunk "Need real tests"
-  end
-end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,4 +13,11 @@ class ActionController::TestCase
   include Devise::TestHelpers
 end
 
+def sign_in(user)
+  visit new_user_session_path
+  fill_in "Email", with: users(user).email
+  fill_in "Password", with: "password"
+  click_on "Log in"
+end
+
 system 'rubocop'
